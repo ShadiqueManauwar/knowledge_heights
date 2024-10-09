9a178cd492be9c69b9c675cde6ef116ec44ad77f
Shadique Manauwar <manauwarshadique07@gmail.com>
2024-10-10 00:23:55 +0530

    adad

--- up_skilling/next.config.mjs
+++ up_skilling/next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint:{
-        ignoreBuildErrors:true
-    },
-    typescript :{
-        ignoreBuildErrors:true
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
